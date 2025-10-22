b78e8dd0e79a825135624be94df1a7f226f55005
Adrián Saul Polanco Ferrer <Adrian>
2025-10-22 19:22:15 -0400

    Performance testing of HttpServer

--- Multithreading/optimizing-throughput-httpserver/src/main/java/org/multithreading/ThroughputHttpServer.java
+++ Multithreading/optimizing-throughput-httpserver/src/main/java/org/multithreading/ThroughputHttpServer.java
@@ -10,8 +10,8 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 public class ThroughputHttpServer {
-    public static String INPUT_FILE = "src/main/resources/war_and_peace.txt";
-    public static int NUMBER_OF_THREADS = 1;
+    public static String INPUT_FILE = "resources/war_and_peace.txt";
+    public static int NUMBER_OF_THREADS = 2;
     public static void startHttpServer(String text) throws IOException {
         HttpServer httpServer = HttpServer.create(new InetSocketAddress(8000), 0);
 
@@ -19,10 +19,5 @@ public class ThroughputHttpServer {
         Executor executor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
         httpServer.setExecutor(executor);
         httpServer.start();
-    }
-
-    @Override
-    public void handle(HttpExchange exchange) throws IOException {
-
     }
 }
